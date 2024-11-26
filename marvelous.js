4b3f5e560207f25e400c9eb7acdb3223ee43d89e
Adeleke-85 <adelekemuinat17@gmail.com>
2024-11-26 17:04:08 +0100

    training-js

---
+++ marvelous.js
@@ -0,0 +1,10 @@
+const myFavouriteColor = ["red", "pink", "green", "white", "yellow" ];
+myFavouriteColor[5] ="orange";
+myFavouriteColor[6] = "gray"
+console.log(myFavouriteColor[0], myFavouriteColor[4])
+myFavouriteColor[1] = "gold";
+
+console.log(myFavouriteColor)
+
+const mynum = [9,8,7,6,5];
+console.log(mynum[2])
---
+++ practice.js
@@ -0,0 +1,22 @@
+const program = {
+    "name" : "nextgen",
+    "duration" : "6 months"
+}
+console.log(program["name"])
+
+const myArray = [1,2,3,4,5,6,7,"8",9,10];
+console.log(myArray[7])
+program["name"] = "nextgen program";
+
+myArray[9] = "something";
+
+console.log(myArray)
+
+
+console.log(program["name"])
+
+console.log(program)
+
+
+
+
